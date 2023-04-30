f1d7fc00e2e82df3d5ea7dc2e0fd0b0d47c49eed
Pankaj Kumar <pankaj.kumar@adda247.com>
2023-04-30 18:32:13 +0530

    removed security code

--- src/main/java/codes/aditya/dynamodb/configuration/AppConfig.java
+++ src/main/java/codes/aditya/dynamodb/configuration/AppConfig.java
@@ -11,9 +11,6 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverterFacto
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -53,16 +50,6 @@ public class AppConfig {
         builder.setConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT);
         builder.withTableNameResolver(DynamoDBMapperConfig.DefaultTableNameResolver.INSTANCE);
         return builder.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
     }
 
 }
